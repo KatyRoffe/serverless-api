ca4ea3bf60a236ccc93e79f9e21d05318e6e2d9f
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-11-11 08:17:39 -0800

    Get method created.

---
+++ get/index.js
@@ -0,0 +1,29 @@
+const dynamoose = require('dynamoose');
+
+exports.handler = async (event) => {
+
+  const peopleSchema = new dynamoose.Schema({
+    'id': Number,
+    'name': String,
+    'age': Number
+  });
+
+  const peopleTable = dynamoose.model('people', peopleSchema);
+
+  let data = null;
+  let status = 500;
+
+  try {
+    data = await peopleTable.scan().exec();
+    status = 200;
+  } catch (error) {
+    data = new Error(error);
+    status = 400;
+  }
+  
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+  return response;
+};
